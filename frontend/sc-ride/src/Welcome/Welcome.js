d6c14bfee2bb7d17a984f6111e35086b6bc5315b
C1330077 Lucas dos Santos Althoff <lsalthoff@stefanini.com>
2023-11-16 15:23:46 -0300

    Versao do questionário pre-release 2

--- frontend/sc-ride/src/Welcome/Welcome.js
+++ frontend/sc-ride/src/Welcome/Welcome.js
@@ -1,8 +1,9 @@
 // Welcome.js
 import React, { useState } from 'react';
-import logo from './logo.png';
+import logo from './logo192.png';
 import './welcomeStyle.css';
-import favicon from './favicon.ico';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faPlay, faSpinner, faCalculator } from '@fortawesome/free-solid-svg-icons'; // Import the specific icons you need
 
 const Welcome = ({ onStartSurvey }) => {
   // const redirectToMaturityCalculation = () => {
@@ -23,32 +24,30 @@ const Welcome = ({ onStartSurvey }) => {
   return (
     <div>
       <div className="header">
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" integrity="sha512-m2fnM5jfvCd/0u1sBI3BBl6Zo6QymPfIuvMIyq2FZGGJ2uEJthblfvVm5klU21Ylk7Ag2qAHCep7IggS41Zf+9A==" crossorigin="anonymous" />
-        <link rel="shortcut icon" href={favicon} />
         <div className="inner-header flex">
           <img src={logo} alt="RIDE" className="logo" width="100" height="100" />
           <div className="header-text">
-          <i className="fas fa-city"></i> Cidades Inteligentes - RIDE <i className="fas fa-city"></i>
+          Cidades Inteligentes - RIDE
           </div>
         </div>
       </div>
 
       <div className="welcome-content flex">
         <div className="welcome-text">
-          <h2><i className="fas fa-clipboard"></i> Bem-vindo ao Questionário de Cidades Inteligentes! <i className="fas fa-clipboard"></i></h2>
+          <h2> Sistema de Apoio à Pesquisa - SmartCity RIDE </h2>
           <p>
-            Obrigado por participar da pesquisa. Clique no botão abaixo para começar a responder.
+          Clique no botão abaixo para começar a responder ou para acessar o diagnóstio parcial de maturidade.
           </p>
           <div className="button-container">
-            <button onClick={onStartSurvey}><i className="fas fa-play"></i> Iniciar Pesquisa</button>
+          <button onClick={onStartSurvey}><FontAwesomeIcon icon={faPlay} /> Iniciar Pesquisa</button>
             <button onClick={redirectToMaturityCalculation}>
-              {loading ? <i className="fas fa-spinner fa-spin"></i> : <i className="fas fa-calculator"></i>} Calcular Maturidade
+              {loading ? <FontAwesomeIcon icon={faSpinner} spin /> : <FontAwesomeIcon icon={faCalculator} />} Calcular Maturidade
             </button>          
           </div>
         </div>
       </div>
       <div className="footer">
-        <p> ®Expo-RIDE 2023 - Ação 1. </p>
+        <p> ®EXPORIDE 2023 - Ação 1. </p>
       </div>
     </div>
   );
